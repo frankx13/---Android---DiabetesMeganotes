a006b4adc556fc486bc60c386528443149dbf9ac
frankx13 <frankx13580@gmail.com>
2019-12-20 00:19:29 +0100

    Made the Alerts' MotionLayout smoother

--- app/src/main/java/com/studio/neopanda/diabetesmeganotes/MyAlertsActivity.java
+++ app/src/main/java/com/studio/neopanda/diabetesmeganotes/MyAlertsActivity.java
@@ -1,15 +1,25 @@
 package com.studio.neopanda.diabetesmeganotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MyAlertsActivity extends AppCompatActivity {
+
+    @BindView(R.id.motion_layout)
+    MotionLayout motionLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerts_motion);
+
+        ButterKnife.bind(this);
+        animate();
     }
 
     private void animate(){
